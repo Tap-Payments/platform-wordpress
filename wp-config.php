dd626c889a47ade0667b11e67a9fdf296e20c8c5
HaithamSheshtawy <h.sheshtawy@tap.company>
2023-04-18 23:03:36 +0300

    enable debg

--- wp-config.php
+++ wp-config.php
@@ -39,6 +39,19 @@ if (!function_exists('getenv_docker')) {
 	}
 }
 
+// Enable WP_DEBUG mode
+define( 'WP_DEBUG', true );
+
+// Enable Debug logging to the /wp-content/debug.log file
+define( 'WP_DEBUG_LOG', true );
+
+// Disable display of errors and warnings
+define( 'WP_DEBUG_DISPLAY', false );
+@ini_set( 'display_errors', 0 );
+
+// Use dev versions of core JS and CSS files (only needed if you are modifying these core files)
+define( 'SCRIPT_DEBUG', true );
+
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define( 'DB_NAME', getenv_docker('WORDPRESS_DB_NAME', 'wordpress') );
